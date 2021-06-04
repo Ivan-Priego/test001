d654d1d75d3226b0098479b15f4f2a5bedc231dc
Ivan-Priego <brauunsimons@gmail.com>
2021-06-03 20:02:10 -0500

    formulario

--- src/componentes/pages/Formulario.js
+++ src/componentes/pages/Formulario.js
@@ -8,6 +8,9 @@ const validate = values => {
     if(!values.pregunta1){
         errors.pregunta1='Esta campo es hobligatorio 1'
     }
+    if(!values.pregunta2){
+        errors.pregunta2='Esta campo es hobligatorio 2'
+    }
     return errors
 }
 
@@ -37,30 +40,32 @@ export default class Formulario extends Component{
     render(){
         const{errors} = this.state
         let cuestionario={
-            preguntas:['¿cual es tu Nombre?','¿Cual es tu apellido?','¿Cual es tu edad?']
+            preguntas:['¿Cual es tu Nombre?','¿Cual es tu apellido?','¿Cual es tu edad?']
         }
         return(
-            <form onSubmit={this.handleSubmit}>
+            <div className="boxForm">
+                <form onSubmit={this.handleSubmit}>
 
-                {
-                    cuestionario.preguntas.map((preguntas,i)=>{
-                        return(
-                            <>
-                            <p key={i}>{preguntas}</p>
-                            <input name={`pregunta${i}`} onChange={this.handleChange} />
-                            {errors.pregunta+[i] && <p>{errors.pregunta0}</p> }
-                            </>
-                        )
-                    })
+                    {
+                        cuestionario.preguntas.map((preguntas,i)=>{
+                            return(
+                                <div className="itemCuestion">
+                                    <p key={i}>{preguntas}</p>
+                                    <input type="text" name={`pregunta${i}`} onChange={this.handleChange} />
+                                    {errors.pregunta0 && <p>{errors.pregunta0}</p> }
+                                </div>
+                            )
+                        })
 
-                }
+                    }
 
-                {/* <input name="nombre" onChange={this.handleChange} />
-                {errors.nombre && <p>{errors.nombre}</p> }
-                <input name="apellido" onChange={this.handleChange} />
-                {errors.apellido && <p>{errors.apellido}</p> } */}
-                <input type="submit" value="enviar" />
-            </form>
+                    {/* <input name="nombre" onChange={this.handleChange} />
+                    {errors.nombre && <p>{errors.nombre}</p> }
+                    <input name="apellido" onChange={this.handleChange} />
+                    {errors.apellido && <p>{errors.apellido}</p> } */}
+                    <input type="submit" value="enviar" />
+                </form>
+            </div>
         )
     };
 }
